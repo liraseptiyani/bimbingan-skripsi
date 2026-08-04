85ddd35b3581d215543f840f8d950f4a761045cf
liraseptiyani <liraseptiyani14@gmail.com>
2026-08-05 05:19:19 +0700

    hash

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -31,6 +31,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        echo "<pre>";
+
+        echo "Username input: ";
+        var_dump($username);
+
+        echo "Password input: ";
+        var_dump($password);
+
+        echo "Data user:\n";
+        var_dump($user);
+
+        if ($user) {
+            echo "Password verify: ";
+            var_dump(password_verify($password, $user['password']));
+        }
+
+        exit;
+
         if ($user) {
 
             // Remember me cookies (only username is saved for security)
