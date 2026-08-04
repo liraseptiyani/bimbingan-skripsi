0354d966eb190f29b3c40fba7306823f0c533794
liraseptiyani <liraseptiyani14@gmail.com>
2026-08-04 14:49:41 +0700

    perubahan 3

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,6 +6,4 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PhpOffice\\PhpWord\\' => array($vendorDir . '/phpoffice/phpword/src/PhpWord'),
-    'PhpOffice\\Math\\' => array($vendorDir . '/phpoffice/math/src/Math'),
 );
